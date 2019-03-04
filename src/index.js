81180e25f2f2824bfb0175be4ad4a158920236eb
Lauren <scps940327@gmail.com>
2019-03-04 23:50:06 +0800

    靜態網頁切版完成
    利用 React 完成分針、時針、秒針的 reander 讓指針會按照時間刷新，達成時鐘的效果

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//import FormContainer from "./js/components/container/FormContainer";
+import Clock from "./js/Clock";
+
+require('./scss/style.scss');
---
+++ src/js/Clock.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Clock extends React.Component {
+   constructor() {
+      super();
+      this.state = {
+         hour: new Date().getHours(),
+         minute: new Date().getMinutes(),
+         second: new Date().getSeconds()
+      }
+
+      setInterval(function(){
+         this.setState({
+            hour: new Date().getHours(),
+            minute: new Date().getMinutes(),
+            second: new Date().getSeconds()
+         }
+      );}.bind(this),1000);
+   }
+   render() {
+      return (
+         <div className="clock">
+            <div id="hour_pointer" className="clock_pointer" style={{transform: 'rotate(' + this.state.hour*30 + 'deg)'}}></div>
+         	<div id="minute_pointer" className="clock_pointer" style={{transform: 'rotate(' + this.state.minute*6 + 'deg)'}}></div>
+            <div id="second_pointer" className="clock_pointer" style={{transform: 'rotate(' + this.state.second*6 + 'deg)'}}></div>
+         </div>
+      );
+   }
+}
+
+export default Clock;
+ReactDOM.render(<Clock />, document.getElementById('root'));
